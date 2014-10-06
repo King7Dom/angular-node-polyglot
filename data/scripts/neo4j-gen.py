b8958fc870d5d72e1b6461de298085e0b8bef8eb
Dominic Cheung <dom.with.a.inic@gmail.com>
2014-10-06 19:55:51 +1100

    Database: Debugged neo4j-gen script

--- data/scripts/neo4j-gen.py
+++ data/scripts/neo4j-gen.py
@@ -8,25 +8,23 @@ with open(neo4j_gen_file, 'w') as gen_file:
 	print 'Setup neo4j-gen.cql'
 	print 'Remember to clear neo4j db before running neo4j-gen.cql'
 	gen_file.write(
-		'MATCH n-[r]-() DELETE n, r;\n'
+		'CREATE CONSTRAINT ON (a:Artist) ASSERT a.id IS UNIQUE;\n'
+		'CREATE INDEX ON :Artist(name);\n'
+		'CREATE CONSTRAINT ON (u:User) ASSERT u.id IS UNIQUE;\n'
+		'CREATE INDEX ON :TAGGED(tag);\n'
 	)
 
 # Generate CREATE queries for Artist nodes
 with open('hetrec2011-lastfm-2k/artists.tsv', 'r') as artist_file:
 	print 'Creating queries for Artist nodes...'
-	with open(neo4j_gen_file, 'a') as gen_file:
-		gen_file.write(
-			'CREATE CONSTRAINT ON (a:Artist) ASSERT a.id IS UNIQUE;\n'
-			'CREATE INDEX ON :Artist(name);\n'
-		)
 
 	artist_file.readline()
 	for line in artist_file:
 		text = line.split('\t')
 		artist_id = text[0].strip()
 		artist_name = text[1].strip().replace('\\', '\\\\').replace('\"', '\\\"').replace('\'', '\\\'')
-		artist_url = text[2].strip()
-		artist_pic_url = text[3].strip()
+		artist_url = text[2].strip().replace('\\', '\\\\').replace('\"', '\\\"').replace('\'', '\\\'')
+		artist_pic_url = text[3].strip().replace('\\', '\\\\').replace('\"', '\\\"').replace('\'', '\\\'')
 
 		query += (
 			'CREATE (:Artist{'
@@ -40,10 +38,6 @@ with open('hetrec2011-lastfm-2k/artists.tsv', 'r') as artist_file:
 # Generate CREATE queries for User nodes and their listening relation with Artist
 with open('hetrec2011-lastfm-2k/user_artists.tsv', 'r') as listen_file:
 	print 'Creating queries for User nodes and their listening relation with Artist...'
-	
-	query += (
-		'CREATE CONSTRAINT ON (u:User) ASSERT u.id IS UNIQUE;\n'
-	)
 
 	listen_file.readline()
 	for line in listen_file:
@@ -55,6 +49,8 @@ with open('hetrec2011-lastfm-2k/user_artists.tsv', 'r') as listen_file:
 		users.add(user_id)
 		query += (
 			'MATCH (a:Artist{id:' + artist_id + '}),(u:User{id:' + user_id + '}) '
+			'USING INDEX u:User(id) '
+			'USING INDEX a:Artist(id) '
 			'CREATE UNIQUE (u)-[:LISTENS_TO{count:'+ listen_count +'}]->(a);\n'
 		)
 
@@ -92,8 +88,6 @@ with open('hetrec2011-lastfm-2k/tags.tsv', 'r') as tag_list:
 # Generate CREATE queries for user tagging relations
 with open('hetrec2011-lastfm-2k/user_taggedartists-timestamps.tsv', 'r') as user_tags_file:
 	print 'Creating queries for user tagging relations...'
-	with open(neo4j_gen_file, 'a') as gen_file:
-		gen_file.write('CREATE INDEX ON :TAGGED(tag)')
 
 	user_tags_file.readline()
 	for line in user_tags_file:
